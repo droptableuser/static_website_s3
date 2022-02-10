e621a813a80e2f1c369023619684a014bc4e5952
Markus <markus@droptableuser.me>
2022-02-11 00:38:37 +0100

    fix env var occurence

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ from static_website_s3.static_website_s3_stack import StaticWebsiteS3Stack
 
 ACCOUNT=os.environ.get('AWS_ACCOUNT', '742344209721')
 REGION=os.environ.get('AWS_REGION', 'eu-central-1')
-DOMAIN=os.environ.get('DOMAIN','droptableuser.me')
 env = core.Environment(account=ACCOUNT, region=REGION)
 app = core.App()
-StaticWebsiteS3Stack(app, "StaticWebsiteS3Stack",DOMAIN, env=env)
+StaticWebsiteS3Stack(app, "StaticWebsiteS3Stack", env=env)
 
 app.synth()
--- static_website_s3/static_website_s3_stack.py
+++ static_website_s3/static_website_s3_stack.py
@@ -18,17 +18,16 @@ from constructs import Construct
 
 class StaticWebsiteS3Stack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, Domain, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, Domain:str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        domain = Domain
+        domain=os.environ.get('DOMAIN','droptableuser.me')
+        
         domain_names = [domain,"www."+domain]
         if os.path.exists("website/public") == False:
             sys.exit("website not found")
 
         bucket = s3.Bucket(self,domain+str(uuid.uuid4()),access_control=s3.BucketAccessControl.PRIVATE,)
-
         s3deploy.BucketDeployment(self,domain+"BucketDeployment",destination_bucket=bucket,sources=[s3deploy.Source.asset("website/public")],retain_on_delete=False)
-
 
         origin_access_identity = cf.OriginAccessIdentity(self,domain+'OriginAccessIdentity')
         bucket.grant_read(origin_access_identity)
@@ -49,6 +48,7 @@ class StaticWebsiteS3Stack(core.Stack):
 
         distribution = cf.Distribution(self,domain+'websitedistribution',       
             default_behavior=cf.BehaviorOptions(origin=origins.S3Origin(bucket,origin_access_identity=origin_access_identity)),domain_names=domain_names,certificate=cert,price_class=cf.PriceClass.PRICE_CLASS_100,error_responses=error_response)
+
         for record in domain_names:
             route53.ARecord(self, record+"Aalias",record_name=record,zone=hosted_zone,target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)))
             route53.AaaaRecord(self, record+"AAAAAalias",record_name=record,zone=hosted_zone,target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)))
