52fd2168589925502e5ec4c2666c2d856e420798
Markus <markus@droptableuser.me>
2022-02-10 22:05:08 +0100

    initial stack

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 import os
 
-import aws_cdk as cdk
+from aws_cdk import core
 
 from static_website_s3.static_website_s3_stack import StaticWebsiteS3Stack
 
-
-app = cdk.App()
-StaticWebsiteS3Stack(app, "StaticWebsiteS3Stack",
+ACCOUNT=os.environ.get('AWS_ACCOUNT', '742344209721')
+REGION=os.environ.get('AWS_REGION', 'eu-central-1')
+DOMAIN=os.environ.get('DOMAIN','droptableuser.me')
+env = core.Environment(account=ACCOUNT, region=REGION)
+app = core.App()
+StaticWebsiteS3Stack(app, "StaticWebsiteS3Stack",DOMAIN, env=env
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
--- static_website_s3/static_website_s3_stack.py
+++ static_website_s3/static_website_s3_stack.py
@@ -1,19 +1,46 @@
+from os import access
+import uuid
 from aws_cdk import (
     # Duration,
-    Stack,
+    core,
+    aws_s3 as s3,
+    aws_cloudfront as cf,
+    aws_cloudfront_origins as origins,
+    aws_route53 as route53,
+    aws_certificatemanager as cm,
+    aws_route53_targets as targets,
     # aws_sqs as sqs,
 )
 from constructs import Construct
 
-class StaticWebsiteS3Stack(Stack):
+class StaticWebsiteS3Stack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, Domain, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        domain = Domain
+        domain_names = [domain,"www."+domain]
 
-        # The code that defines your stack goes here
+        bucket = s3.Bucket(self,domain+str(uuid.uuid4()),access_control=s3.BucketAccessControl.PRIVATE)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "StaticWebsiteS3Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        origin_access_identity = cf.OriginAccessIdentity(self,domain+'OriginAccessIdentity')
+        bucket.grant_read(origin_access_identity)
+
+        hosted_zone = route53.HostedZone.from_lookup(
+             self, domain+"HostedZone", domain_name=domain)
+
+        cert = cm.Certificate(
+             self, domain+"Certificate",
+             domain_name=domain,
+             subject_alternative_names=domain_names,
+             validation=cm.CertificateValidation.from_dns(hosted_zone))
+        
+        error_response = [cf.ErrorResponse(
+            http_status=404,
+            response_page_path="/404.html")]
+
+
+        distribution = cf.Distribution(self,domain+'websitedistribution',       
+            default_behavior=cf.BehaviorOptions(origin=origins.S3Origin(bucket,origin_access_identity=origin_access_identity)),domain_names=domain_names,certificate=cert,price_class=cf.PriceClass.PRICE_CLASS_100,error_responses=error_response)
+        for record in domain_names:
+            route53.ARecord(self, record+"Aalias",record_name=record,zone=hosted_zone,target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)))
+            route53.AaaaRecord(self, record+"AAAAAalias",record_name=record,zone=hosted_zone,target=route53.RecordTarget.from_alias(targets.CloudFrontTarget(distribution)))
