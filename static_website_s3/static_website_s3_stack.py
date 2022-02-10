5820df722ec0f2c22cd01b7d8b4188ebb04da2ff
Markus <markus@droptableuser.me>
2022-02-11 00:41:55 +0100

    fix env var occurence

--- static_website_s3/static_website_s3_stack.py
+++ static_website_s3/static_website_s3_stack.py
@@ -18,7 +18,7 @@ from constructs import Construct
 
 class StaticWebsiteS3Stack(core.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, Domain:str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         domain=os.environ.get('DOMAIN','droptableuser.me')
         
