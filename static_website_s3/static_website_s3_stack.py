23b7bdea2ff8de1b96dcf71b295d14f0846e4d17
Markus <markus@droptableuser.me>
2023-02-06 16:20:04 +0100

    cant reade

--- static_website_s3/static_website_s3_stack.py
+++ static_website_s3/static_website_s3_stack.py
@@ -42,7 +42,7 @@ class StaticWebsiteS3Stack(Stack):
         origin_access_identity = cf.OriginAccessIdentity(self,domain+'OriginAccessIdentity')
         bucket.grant_read(origin_access_identity)
 
-        hosted_zone = route53.HostedZone.from_hosted_zone_attribute(
+        hosted_zone = route53.HostedZone.from_hosted_zone_attributes(
              self, "DomainHostedZone","Z06012823NBMI91D46AY5","scharitzer.io")
 
         cert = cm.DnsValidatedCertificate(
